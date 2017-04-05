a5a5d740381dcdee2dd6e22bc82b96dc2d9699d9
toxa14147 <kaftanov1212@mail.ru>
2017-04-05 22:51:31 +0300

    вывод точек на экран,применение функций

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write( sym1 );
+           
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
             Console.ReadLine();
         }
     }
