5207b8ad1be2246178b5d89a78af0c4d7becd03a
toxa14147 <kaftanov1212@mail.ru>
2017-04-06 22:47:01 +0300

    создан класс поинт и метод инкапсуляции

--- snake/snake/point.cs
+++ snake/snake/point.cs
@@ -11,8 +11,18 @@ namespace snake
         public int x;
         public int y;
         public char sym;
-        public void Draw()
+
+        public point()
         {
+        }
+        public point(int _x,int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+     public void Draw()
+    {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
