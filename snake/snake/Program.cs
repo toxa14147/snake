e92558231bb4077ba9603a208d989a17615f2f8b
toxa14147 <kaftanov1212@mail.ru>
2017-04-07 22:44:29 +0300

    сделано дз.проведены эксперименты со списками

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -17,22 +17,47 @@ namespace snake
             point p2 = new point(4, 5,'#');
             
             p2.Draw();
-
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-            foreach(int i in numlist )
+            //point p3 = new point(6, 8, '*');
+            //point p4 = new point(7, 10, '#');
+            char sym1 = '*';
+            char sym2 = '#';
+            List<char> sym = new List<char>();
+            sym.Add(sym1);
+            sym.Add(sym2);
+            var a = sym[0];
+            var b = sym[1];
+            foreach(var i in sym)
             {
                 Console.WriteLine(i);
             }
-            numlist.RemoveAt(0);
-            List<point> plist = new List<point>();
-            plist.Add(p1);
-            plist.Add(p2);
+            //List<int> numlist = new List<int>();
+            //numlist.Add(0);
+            //numlist.Add(1);
+            //numlist.Add(2);
+            //int x = numlist[0];
+            //int y = numlist[1];
+            //int z = numlist[2];
+            //foreach(int i in numlist )
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //numlist.RemoveAt(0);
+            //List<point> plist = new List<point>();
+            //plist.Add(p1);
+            //plist.Add(p2);
+            //Console.ReadLine();
+            //List<point> plist = new List<point>();
+            //plist.Add(p1);
+            //plist.Add(p2);
+            //plist.Add(p3);
+            //plist.Add(p4);
+            //point p = plist[0];
+            //point a = plist[1];
+            //point e = plist[2];
+            //foreach(point i in plist)
+            //{
+            //    Console.WriteLine(i);
+            //}
             Console.ReadLine();
         }
        
