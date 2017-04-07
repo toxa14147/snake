8b70fd630f6e6a846c99325f98560824b2fe9c36
toxa14147 <kaftanov1212@mail.ru>
2017-04-07 23:12:41 +0300

    создаем линии

---
+++ snake/snake/HorisontalLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorisontalLine
+    {
+        List<point> plist;
+        public HorisontalLine(int yLeft,int yRight,int x,char sym)
+        {
+            plist = new List<point>();
+            for(int y = yLeft; y <= yRight; y++) {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach(point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
