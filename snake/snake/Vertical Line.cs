d64f43bd2d4264758eb253897066029c6009914b
toxa14147 <kaftanov1212@mail.ru>
2017-04-09 19:33:33 +0300

    рамочка

---
+++ snake/snake/Vertical Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class horisontalline
+    {
+        List<point> plist;
+        public horisontalline(int xleft, int xright, int y, char sym)
+        {
+            plist = new List<point>();
+            for (int x = xleft; x <= xright; x++)
+            {
+                point p = new point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach (point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
